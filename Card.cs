9b93b97704b8079310f14349b48bb5eb4535f5ba
colecrawley <26430829@students.lincoln.ac.uk>
2023-03-07 14:17:08 +0000

    second commit, error with running build, reinstall

--- Card.cs
+++ Card.cs
@@ -12,7 +12,16 @@ namespace CMP1903M_A01_2223
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+
+
+        public int[] cardvalues = {1,2,3,4,5,6,7,8,9,10,12,13};
+        public int[] suitvalues = {1,2,3,4};
+
+
+        public int Value
+        {get; set;}
+        
+        public int Suit
+        {get; set;}
     }
 }
--- Pack.cs
+++ Pack.cs
@@ -6,18 +6,76 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
-    class Pack
+    class Pack : Card
     {
-        List<Card> pack;
+        int total_cards = 52; //total number of playing cards
+        private Card[] deck; // all of the playing cards, private so the cards don't get changed
 
+        List<Card> pack;
         public Pack()
         {
             //Initialise the card pack here
+
+            deck = new Card[total_cards];
         }
 
+        public Card[] getpack
+        {
+
+            get
+            {
+                return deck; // get deck
+            }
+             
+        }
+        
+        public void decksetup() // creation of cards
+        {
+
+            for (int i = 0; i < suitvalues.Length; i++)
+            {
+                for (int j = 0; i < cardvalues.Length; i++);
+                {
+                    deck[i][j] = new Card {Suit = i, Value = j};
+                }
+            }
+
+        }
+        
         public static bool shuffleCardPack(int typeOfShuffle)
         {
             //Shuffles the pack based on the type of shuffle
+            // 1. Fisher-Yates shuffle
+            // 2. Riffle shuffle
+            // 3. No shuffle
+
+
+            // PUT THIS IN A LOOP
+            System.Console.WriteLine("Choose what type of shuffle you want to use: \n1. Fisher-Yates shuffle\n2. Riffle Shuffle\n3. No Shuffle\n");
+
+            try // not sure if the user input will automaticaly be int or string
+            {
+                int typeOfShuffle = Console.ReadLine();
+            }
+            catch
+            {
+                int.Parse(typeOfShuffle);
+            }
+            
+            if (typeOfShuffle == "1")
+            {
+                typeOfShuffle = "Fisher-Yates Shuffle";
+            }
+            if (typeOfShuffle == "2")
+            {
+                typeOfShuffle = "Riffle Shuffle";
+            }
+            if (typeOfShuffle == "3")
+            {
+                typeOfShuffle = "No Shuffle";
+            }
+
+            System.Console.WriteLine("You have chosen " + usershuffle);
 
         }
         public static Card deal()
